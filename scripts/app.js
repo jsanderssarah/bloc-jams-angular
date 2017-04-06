e29b960f54ac7770dcd760edbf2e253fe3e77313
Sarah Bentzen <Sarah@Sarahs-MacBook-Pro.local>
2017-04-06 14:11:47 -0500

    corrections

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,26 @@
-(function() {
-  function config($stateProvider, $locationProvider) {   
-    $locationProvider
-      .html5Mode({
-        enabled: true,
-        requireBase: false
-      });
-    $stateProvider
-      .state('landing', {
-        url: '/',
-        templateUrl: '/templates/landing.html'
-      })
-      .state('album', {
-        url: '/album',
-        templateUrl: '/templates/album.html'
-      })
-      .state('collection', {
-        url: '/collection',
-        templateUrl: '/templates/collection.html'
-      });
-    angular
-      .module('bloc-jams', ['ui.router']);
-      .config(config);
-  }
-})();
+((function() {
+     function config($stateProvider, $locationProvider) {
+       $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+       $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
+          })
+         .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         })
+         .state('collection', {
+           url: '/collection',
+           templateUrl: '/templates/collection.html'
+       });
+     } 
+     angular
+       .module('blocJams', ['ui.router'])
+       .config(config);
+ })();
+   
