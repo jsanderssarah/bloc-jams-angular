c1e3972a58d5b15509dac04b541673851542a213
Sarah Bentzen <Sarah@Sarahs-MacBook-Pro.local>
2017-04-10 10:35:11 -0500

    Checkpoint 5 Controllers complete

--- scripts/controllers/LandingCtrl.js
+++ scripts/controllers/LandingCtrl.js
@@ -1,7 +1,8 @@
 (function() {
- function LandingCtrl() {     
- }   
- angular
-   .module('blocJams')
-   .controller('LandingCtrl', LandingCtrl);
-})();
+     function LandingCtrl() {
+         this.heroTitle = "Turn the Music Up!";
+     }
+     angular
+         .module('blocJams')
+         .controller('LandingCtrl', LandingCtrl);
+ })();
